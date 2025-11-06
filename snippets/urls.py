ed79e357f3fea1cba47cc6722aa80a7d4c62b5fe
Alvi305 <alvirahman305@gmail.com>
2025-11-06 19:10:21 +0800

    feat: restAPI request and response core, view wrappers, format negotiation, and format suffixes

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import snippet_views
 
 urlpatterns = [
     path("snippets/", snippet_views.snippet_list),
     path("snippets/<int:pk>/", snippet_views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
